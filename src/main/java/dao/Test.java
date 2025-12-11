5584679e1830e6e383de8f89f9efb38cdff7447d
TyntyNguyen0710 <philong2m@gmail.com>
2025-12-11 19:23:25 +0700

    Add Test.java placeholder in dao package
    Created a new Test.java file in the dao package with commented-out code for future testing of bookingDAO and Booking functionality.

---
+++ src/main/java/dao/Test.java
@@ -0,0 +1,12 @@
+package dao;
+
+//import model.Booking;
+
+public class Test {
+//	public static void main(String[] args) {
+//		bookingDAO bookingdao = bookingDAO.getIntance();
+//		Booking booking = bookingdao.selectByID(email);
+//		System.out.println(booking);
+//
+//	}
+}
