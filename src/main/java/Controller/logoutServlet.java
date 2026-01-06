279dc67b75293985ac245bcc398acb4a27985d45
TyntyNguyen0710 <philong2m@gmail.com>
2026-01-06 13:13:36 +0700

    Add logoutServlet for user session logout
    Introduces logoutServlet to handle user logout by invalidating sessions and redirecting to 'trangchu.jsp'. Also includes a scheduled task for periodic session invalidation.

---
+++ src/main/java/Controller/logoutServlet.java
@@ -0,0 +1,58 @@
+package Controller;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/logout")
+public class logoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public logoutServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession(false);
+		if (session != null) {
+			session.invalidate();
+		}
+		response.sendRedirect("trangchu.jsp");
+	}
+
+	@Override
+	public void init() throws ServletException {
+		Timer timer = new Timer(true);
+
+		// Thêm công việc định kỳ với Timer
+		timer.scheduleAtFixedRate(new LogoutTask(), 30 * 60 * 1000, 30 * 60 * 1000);
+	}
+
+	class LogoutTask extends TimerTask {
+		@Override
+		public void run() {
+			HttpSession session = getSession();
+			if (session != null) {
+				session.invalidate();
+			}
+		}
+
+		private HttpSession getSession() {
+			// Lấy session từ ServletContext
+			return ((HttpServletRequest) getServletContext()).getSession(false);
+		}
+	}
+}
