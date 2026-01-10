1cb892b16b5ce14d9d7a85294951e480fada5fcf
TyntyNguyen0710 <philong2m@gmail.com>
2026-01-10 18:36:36 +0700

    Add ChangeInformationFilter for authentication check
    Introduces a servlet filter that restricts access to changeInformation.jsp to authenticated users. Unauthenticated users are redirected to trangchu.jsp.

---
+++ src/main/java/Filter/ChangeInformationFilter.java
@@ -0,0 +1,41 @@
+package Filter;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter("/changeInformation.jsp")
+public class ChangeInformationFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession session = httpRequest.getSession(false);
+
+        String username = (session != null) ? (String) session.getAttribute("username") : null;
+
+        if (username != null) {
+            chain.doFilter(request, response);
+        } else {
+
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/trangchu.jsp");
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
