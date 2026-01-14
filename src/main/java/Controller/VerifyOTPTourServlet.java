928d419cffa6c77f0c94936e0ecab315f4a3c156
TyntyNguyen0710 <philong2m@gmail.com>
2026-01-14 10:50:51 +0700

    verify otp tour

---
+++ src/main/java/Controller/VerifyOTPTourServlet.java
@@ -0,0 +1,38 @@
+package Controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class VerifyOTPTourServlet
+ */
+@WebServlet("/VerifyOTPTourServlet")
+public class VerifyOTPTourServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Lấy giá trị OTP nhập từ trang verifyOTP.jsp
+        String enteredOTP = request.getParameter("otp");
+
+        // Lấy giá trị OTP từ session
+        HttpSession session = request.getSession();
+        String storedOTP = (String) session.getAttribute("otp");
+
+        // Kiểm tra xem OTP nhập vào có khớp với OTP từ session hay không
+        if (enteredOTP != null && storedOTP != null && enteredOTP.equals(storedOTP)) {
+            // Xác nhận thành công
+
+            // Chuyển hướng đến trang checkingTourNoLoginFinal
+            response.sendRedirect("checkingTourNoLoginFinal.jsp");
+        } else {
+            // Xác nhận thất bại
+            response.sendRedirect("verifyOTPRegain.jsp");
+        }
+    }
+}
