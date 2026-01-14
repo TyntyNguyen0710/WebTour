52574eb601682ae6648421e01aed00f42e8ec77f
TyntyNguyen0710 <philong2m@gmail.com>
2026-01-14 10:51:54 +0700

    otp filter

---
+++ src/main/java/Filter/OTPTourConfirmationFilter.java
@@ -0,0 +1,46 @@
+package Filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter("/verifyOTP.jsp")
+public class OTPTourConfirmationFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        // Kiểm tra xem giá trị của session.getAttribute("otp") có bằng null hay không
+        HttpSession session = httpRequest.getSession(false);
+
+        if (session == null || session.getAttribute("otp") == null) {
+            // Nếu giá trị bằng null hoặc không có, chuyển hướng đến trang checkingTourNoLogin.jsp
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/checkingTourNoLogin.jsp");
+        } else {
+            // Giá trị tồn tại, cho phép tiếp tục xử lý request
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Không cần thiết trong trường hợp này
+    }
+
+    @Override
+    public void destroy() {
+        // Không cần thiết trong trường hợp này
+    }
+}
